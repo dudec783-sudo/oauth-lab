fca2a915d0cb22f7110d4edba089de9b2f637a3d
Cool <dudec783@gmail.com>
2026-04-19 11:07:11 +0530

    show user info

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
+import requests
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
+    user = None
+
+    # Call Azure Easy Auth endpoint
+    headers = {"X-ZUMO-AUTH": request.headers.get("X-ZUMO-AUTH", "")}
+    
+    try:
+        resp = requests.get(
+            os.environ.get("WEBSITE_AUTH_ME_ENDPOINT", "/.auth/me"),
+        )
+        data = resp.json()
+        if data:
+            user = data[0]
+    except:
+        pass
+
+    return f"""
     <h2>OAuth Lab</h2>
-    <a href="/login">Login with Microsoft</a>
+    <a href="/login">Login with Microsoft</a><br><br>
+
+    <h3>User Info:</h3>
+    <pre>{user}</pre>
     """
 
 @app.route("/login")
