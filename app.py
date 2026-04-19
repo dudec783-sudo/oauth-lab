936d89eb44bc3a2beaa704b39f01f3d16b80491d
Cool <dudec783@gmail.com>
2026-04-19 11:49:39 +0530

    remove blocking call

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-from flask import Flask, redirect, request
-import requests
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    user = None
-
-    try:
-        # Call Azure Easy Auth endpoint
-        resp = requests.get("http://127.0.0.1:8000/.auth/me")
-        data = resp.json()
-        if data:
-            user = data[0]
-    except Exception as e:
-        user = f"Error: {str(e)}"
-
-    return f"""
+    return """
     <h2>OAuth Lab</h2>
-    <a href="/login">Login with Microsoft</a><br><br>
-
-    <h3>User Info:</h3>
-    <pre>{user}</pre>
+    <a href="/login">Login with Microsoft</a>
     """
 
 @app.route("/login")
